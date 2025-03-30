c9a2623cc9a2c810c4dce537c2c4d43f59fa2eed
IhorBohush <boigor95@gmail.com>
2025-03-31 00:31:58 +0300

    changes in accordion

---
+++ js/index.crafted.js
@@ -0,0 +1,5 @@
+// const craftedAccordion = document.querySelector('.crafted__accordion--icon').addEventListener('click', craftedFormContainer);
+// const craftedAccordionIcon = document.querySelector('.crafted__accordion--icon', '::after');
+// function craftedFormContainer() {
+//     craftedAccordionIcon.style.opacity = 0;
+// }
