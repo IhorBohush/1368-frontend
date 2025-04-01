f9b78f67d47906a7e5bf44b3aaef9ff413d2aeee
IhorBohush <boigor95@gmail.com>
2025-04-02 01:00:49 +0300

    add form to my block

--- js/index.crafted.js
+++ js/index.crafted.js
@@ -1,7 +1,9 @@
-const craftedAccordion = document.querySelector('.crafted__accordion--item').addEventListener('click', craftedFormContainer);
+const craftedAccordionItem = document.querySelector('.crafted__accordion--item').addEventListener('click', craftedFormContainer);
+const craftedAccordion = document.querySelector('.crafted__accordion');
 const craftedAccordionIcon = document.querySelector('.crafted__accordion--icon');
-const craftedAccordionForm = document.querySelector('.crafted__accordion--form')
+const craftedAccordionForm = document.querySelector('.crafted__accordion--form');
 function craftedFormContainer() {
     craftedAccordionIcon.classList.toggle('active');
     craftedAccordionForm.classList.toggle('active');
+    craftedAccordion.classList.toggle('active');
 }
