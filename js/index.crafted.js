8813224bd1e17a4b05be73a1178101f725da82c3
IhorBohush <boigor95@gmail.com>
2025-04-04 07:01:56 +0300

    changes in according

--- js/index.crafted.js
+++ js/index.crafted.js
@@ -7,3 +7,43 @@ function craftedFormContainer() {
     craftedAccordionForm.classList.toggle('active');
     craftedAccordion.classList.toggle('active');
 }
+
+const craftedForm = document.getElementById('craftedform');
+const craftedResult = document.getElementById('craftedresult');
+
+craftedForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(craftedForm);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  craftedResult.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                craftedResult.innerHTML = json.message;
+            } else {
+                console.log(response);
+                craftedResult.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            craftedResult.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            craftedForm.reset();
+            setTimeout(() => {
+                craftedResult.style.visibility = "hidden";
+            }, 3000);
+            craftedResult.style.visibility = "visible";
+        });
+});
